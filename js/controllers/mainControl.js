d057ef13484633e6be03d90217acfed4df60f499
Malorie Kramer <mkramer2089@gmail.com>
2016-07-21 22:33:38 -0400

    in api

--- js/controllers/mainControl.js
+++ js/controllers/mainControl.js
@@ -6,5 +6,17 @@
     .controller('MainController', function(API) {
        var vm = this;
       
+       var showInfo = function(){
+	       var showConcert = API.getCinci();
+
+	       showConcert.then(function(results){
+	       		// var games = results.data.__metadata.title;
+	       		console.log(results);
+	       		vm.concerts = results.data.events;
+	       })
+		}       
+      	
+		showInfo();
+
     });
 })();
--- js/factories/api.js
+++ js/factories/api.js
@@ -4,18 +4,31 @@
 		.module('mapChart')
 		 .factory('API', function($http) {
 
-		 	function getGames(){
-		        var gettingGames = $http({
+		 	// function getConcert(){
+		  //       var gettingInfo = $http({
+		  //         method: 'GET',
+		  //         url: 'http://api.jambase.com/events?zipCode=94054&api_key=p5wvt8qjumq3dr7wqwjaauuh',
+		  //         });
+
+		  //       return gettingInfo;
+   	// 		}
+		 
+   	// 		return {
+   	// 			getConcert:getConcert,
+   	// 		}
+
+   			function getCinci(){
+		        var gettingCinci = $http({
 		          method: 'GET',
-		          url: 'http://api.jambase.com/events?zipCode=94054&api_key=p5wvt8qjumq3dr7wqwjaauuh',
+		          url: 'http://api.jambase.com/events?zipCode=45209&page=0&api_key=p5wvt8qjumq3dr7wqwjaauuh',
 		          });
 
-		        return gettingGames;
+		        return gettingCinci;
    			}
 		 
    			return {
-   				getGames:getGames,
+   				// getConcert:getConcert,
+   				getCinci:getCinci,
    			}
-
 		 });	
 })();
